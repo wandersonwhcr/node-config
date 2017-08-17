ad6018cc35fec83742d5e7db474d0f90314e390e
Wanderson <wandersonwhcr@gmail.com>
2017-08-16 22:12:21 -0300

    Possibilita configuração de padrões

--- test/Config.js
+++ test/Config.js
@@ -1,3 +1,17 @@
 "use strict";
 
+const assert = require("assert");
 const Config = require("../lib/Config.js");
+
+describe("Config", function () {
+    describe("Pattern", function () {
+        it("uses encapsulation", function () {
+            var config = new Config();
+            assert.equal(config.getPattern(), undefined);
+            assert.equal(config.setPattern(["foo.json"]), undefined);
+            assert.deepEqual(config.getPattern(), ["foo.json"]);
+            assert.equal(config.setPattern(), undefined);
+            assert.equal(config.getPattern(), undefined);
+        });
+    });
+});
