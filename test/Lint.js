f6359a8b01d2f573f0b15c71313ee55457bd4908
Wanderson <wandersonwhcr@gmail.com>
2017-09-02 15:42:34 -0300

    Configura tempo limite para teste unitário

--- test/Lint.js
+++ test/Lint.js
@@ -4,4 +4,4 @@ const lint = require('mocha-eslint');
 
 const paths = ['index.js', 'lib', 'test'];
 
-lint(paths, { contextName: 'Lint' });
+lint(paths, { contextName: 'Lint', timeout: Number.MAX_SAFE_INTEGER });
